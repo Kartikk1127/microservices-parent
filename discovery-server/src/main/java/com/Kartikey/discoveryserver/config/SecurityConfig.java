42cc34b0cd08a9ad20b5aa69c31b78212d319d54
Kartikey <kartikey.srivastava1271@gmail.com>
2023-05-10 01:47:53 +0530

    "implemented circuit breaker"

--- discovery-server/src/main/java/com/Kartikey/discoveryserver/config/SecurityConfig.java
+++ discovery-server/src/main/java/com/Kartikey/discoveryserver/config/SecurityConfig.java
@@ -2,18 +2,33 @@ package com.Kartikey.discoveryserver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
-
-        httpSecurity.csrf().ignoringAntMatchers("/eureka/**");
-        return httpSecurity.build();
-
+    @Override
+    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.inMemoryAuthentication()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .withUser("eureka").password("password")
+                .authorities("USER");
     }
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                .authorizeRequests().anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
 }
